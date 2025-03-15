44f68f8e03ca94b533b6bb7d7c119748a5d7f126
konrad <kedziora.adres@gmail.com>
2025-03-15 13:56:43 +0100

    Commit test

--- src/main/java/pl/wsb/FitnessTracker.java
+++ src/main/java/pl/wsb/FitnessTracker.java
@@ -8,6 +8,7 @@ public class FitnessTracker {
 
     public static void main(String[] args) {
         SpringApplication.run(FitnessTracker.class, args);
+        System.out.println("Test");
     }
 
 }
